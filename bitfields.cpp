6502656a0056a9a1ef3b544206ebca7ae5c9d12a
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2015-08-04 14:03:58 +0200

    [bitfields] use enum in bitfield

--- bitfields.cpp
+++ bitfields.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
 #include <stdint.h>
 
+enum class Color : uint8_t
+{
+	Red = 0,
+	Green = 1,
+	Blue = 2,
+	Black = 100,
+};
+
+inline std::ostream& operator<<(std::ostream & os, const Color & color) {
+	switch(color) {
+	case Color::Red:
+		os << "Red"; break;
+	case Color::Green:
+		os << "Green"; break;
+	case Color::Blue:
+		os << "Blue"; break;
+	}
+	return os;
+}
+
 struct Fields
 {
 	unsigned int a : 4;
 	signed   int b : 4;
 	uint16_t     c : 4;
 	int16_t      d : 4;
+	Color        color : 2;
 };
 
 #define TEST(value) \
@@ -23,12 +44,18 @@ int main()
 	std::cout << "unsigned int a : 4;" << std::endl;
 	TEST(ff.a)
 
-	std::cout << "signed   int b : 4;" << std::endl;
+	std::cout << "signed int b : 4;" << std::endl;
 	TEST(ff.b)
 
-	std::cout << "uint16_t     c : 4;" << std::endl;
+	std::cout << "uint16_t c : 4;" << std::endl;
 	TEST(ff.c)
 
-	std::cout << "int16_t      c : 4;" << std::endl;
+	std::cout << "int16_t c : 4;" << std::endl;
 	TEST(ff.d)
+
+	std::cout << "Color color : 2;" << std::endl;
+	ff.color = Color::Green;
+	std::cout << "ff.color: " << ff.color << std::endl;
+	ff.color = Color::Black;
+	std::cout << "ff.color: " << ff.color << std::endl;
 }
